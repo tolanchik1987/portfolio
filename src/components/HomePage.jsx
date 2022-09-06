3f4d1422b0f2505a57459b109cda6ef60ece9228
tolanchik1987 <tolanchik1987@mail.ru>
2022-09-06 11:26:15 +0400

    creat HomPage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./HomePage.module.scss"
+
+const HomePage = () => {
+  return (
+    <div className={classes.conteiner}>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage;
